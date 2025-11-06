39f98c7054d6240bf9110891a6283219ae5a56eb
AnhPhung-AIOZAI <anh.phung-tuan.pham@aioz.io>
2025-11-06 11:01:41 +0700

    update: add optimizer

--- js/modules/param/optimizers.js
+++ js/modules/param/optimizers.js
@@ -4,22 +4,25 @@ export const AVAILABLE_OPTIMIZERS = {
         params: [
             { name: "lr", label: "Learning Rate", type: "number", default: 0.01 },
             { name: "momentum", label: "Momentum", type: "number", default: 0.9 },
-            { name: "weight_decay", label: "Weight Decay", type: "number", default: 0 }
+            { name: "weight_decay", label: "Weight Decay", type: "number", default: 0 },
+            { name: "nesterov", label: "Nesterov", type: "bool", default: true }
         ]
     },
     Adam: {
         label: "Adam",
         params: [
             { name: "lr", label: "Learning Rate", type: "number", default: 0.001 },
-            { name: "betas", label: "Betas", type: "text", default: "(0.9, 0.999)" },
-            { name: "weight_decay", label: "Weight Decay", type: "number", default: 0 }
+            { name: "betas", label: "Betas", type: "text", default: "[0.9, 0.999]" },
+            { name: "weight_decay", label: "Weight Decay", type: "number", default: 0 },
+            { name: "eps", label: "Eps", type: "number", default: 1e-8 }
         ]
     },
     AdamW: {
         label: "AdamW",
         params: [
             { name: "lr", label: "Learning Rate", type: "number", default: 0.001 },
-            { name: "betas", label: "Betas", type: "text", default: "(0.9, 0.999)" },
+            { name: "betas", label: "Betas", type: "text", default: "[0.9, 0.999]" },
+            { name: "eps", label: "Eps", type: "number", default: 1e-8 },
             { name: "weight_decay", label: "Weight Decay", type: "number", default: 0.01 }
         ]
     },
@@ -29,6 +32,16 @@ export const AVAILABLE_OPTIMIZERS = {
             { name: "lr", label: "Learning Rate", type: "number", default: 0.01 },
             { name: "momentum", label: "Momentum", type: "number", default: 0 },
             { name: "alpha", label: "Alpha", type: "number", default: 0.99 },
+            { name: "weight_decay", label: "Weight Decay", type: "number", default: 0 },
+            { name: "eps", label: "Eps", type: "number", default: 1e-8 },
+        ]
+    },
+    Adamax: {
+        label: "Adamax",
+        params: [
+            { name: "lr", label: "Learning Rate", type: "number", default: 0.01 },
+            { name: "betas", label: "Betas", type: "text", default: "[0.9, 0.999]" },
+            { name: "eps", label: "Eps", type: "number", default: 1e-8 },
             { name: "weight_decay", label: "Weight Decay", type: "number", default: 0 }
         ]
     }
